5b0f6f78073228b516493a7b2bcf8e125dd9850d
Rafael-Tinelli <166090180+Rafael-Tinelli@users.noreply.github.com>
2026-01-30 12:33:17 -0300

    Update App.jsx

--- widget-ui/src/App.jsx
+++ widget-ui/src/App.jsx
@@ -89,7 +89,6 @@ export default function App() {
 
     return () => {
       window.removeEventListener("resize", updateStickyTop);
-      window.removeEventListener("scroll", updateStickyTop);
       cancelAnimationFrame(raf);
       timers.forEach(clearTimeout);
     };
