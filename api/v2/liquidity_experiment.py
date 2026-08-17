12507d4321e788868332babc9b53464fdd3b91c8
Rafael-Tinelli <166090180+Rafael-Tinelli@users.noreply.github.com>
2026-08-17 20:59:55 -0300

    fix(v2): clean liquidity experiment lint

--- api/v2/liquidity_experiment.py
+++ api/v2/liquidity_experiment.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 import math
 from collections import Counter
 from copy import deepcopy
-from statistics import mean, median, pstdev
+from statistics import median
 from typing import Any
 
 import numpy as np
